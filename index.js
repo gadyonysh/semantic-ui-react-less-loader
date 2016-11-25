f9d7c7e351991c2124e87a089058ee25545380e9
Vladimir Tazin <vladimir@wildapricot.com>
2016-11-25 11:38:59 +0300

    Remove redundant styleMode

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 var fs = require('fs');
-var componentRegex = /([^\W]+)[\\\/]semantic-ui-react[\\\/]src[\\\/](elements|collections|modules|views)[\\\/]([a-zA-Z0-9-]+)[\\\/]index.jsx?$/;
+var componentPathRegex = /([^\W]+)[\\\/]semantic-ui-react[\\\/]src[\\\/](elements|collections|modules|views)[\\\/]([a-zA-Z0-9-]+)[\\\/]index.jsx?$/;
 
-function getStyleMode(query)
-{
-  return (styleModes.indexOf(query) === -1) ? defaultStyleMode : query;
-}
-
-function getStylePath(definition, component, styleMode)
+function getStylePath(definition, component)
 {
   return 'semantic-ui-less/definitions/' + definition + '/' + component.toLowerCase() + '.less';
 }
@@ -20,16 +15,14 @@ module.exports = function(source, map)
 {
   this.cacheable();
 
-  var matches = this.resourcePath.match(componentRegex);
+  var matches = this.resourcePath.match(componentPathRegex);
   var fsPath;
   var path;
-  var styleMode;
 
   if (matches && matches.length === 4)
   {
     fsPath = matches[1].replace('\\', '/');
-    styleMode = getStyleMode(this.resourceQuery);
-    path = getStylePath(matches[2], matches[3], styleMode);
+    path = getStylePath(matches[2], matches[3]);
 
     if (fs.existsSync(fsPath + '/' + path))
     {
